de802f9d9ddb6401e86a02ef29d73d6934514f54
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-29 20:07:51 +0400

    add node modules, index and server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('HELLO!!!')
+})
+
+app.listen(8080, function () {
+  console.log('Server listening on port 8080!')
