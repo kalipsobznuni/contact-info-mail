f1bf617d7a192d656be373c74c45207493d86d15
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-30 02:13:07 +0400

    add files

--- homepage.jsx
+++ homepage.jsx
@@ -2,3 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+const { Component } = React;
+
+class App extends Component {
+
+  constructor(){
+
+  }
+  
+  <div id="react-container"></div>
+  <script type="text/babel">
