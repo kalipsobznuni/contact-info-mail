65eb7e013fe179447e9c8c9d2a99a3879d40a858
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-30 07:01:07 +0400

    add time as a comment

--- lib/homepage.jsx
+++ lib/homepage.jsx
@@ -5,11 +5,25 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render(){
-    return(
+    return
   <h1> HELLO!!!</h1>
-);
+
 }
 }
+/*function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+*/
+setInterval(tick, 1000);
 
 const container = document.getElementById("container");
 
