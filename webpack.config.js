70ae668ac3323a7e6d5ee5215b69fed9bff3f672
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2016-11-30 05:09:54 +0400

    new webpack js->jsx +bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
-
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
-    ]
+  entry: './lib/homepage.jsx',
+  devServer:{
+    inline:true,
+    progress:true,
+    contentBase:'public',
+    historyApiFallback: true
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    path: `${__dirname}/public`,
+    filename: 'bundle.js'
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module:{
+    loaders:[{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      exclude:/node_modules/,
+      include: `${__dirname}/lib`,
+      query: {
+        // Here you can put plugins, like plugins:['transform-runtime']
+        presets: ['es2015', 'react', 'stage-3', 'stage-0']
+      }
+    }]
+  }
 };
